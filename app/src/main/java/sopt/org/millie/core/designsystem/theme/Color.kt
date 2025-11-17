4eded4120b73128afc3406d4d155d17e95f1cf9d
너의이름 <qkrcksal1052@gmail.com>
2025-11-17 13:08:31 +0900

    [feat/#3] designsystem setting

--- app/src/main/java/sopt/org/millie/core/designsystem/theme/Color.kt
+++ app/src/main/java/sopt/org/millie/core/designsystem/theme/Color.kt
@@ -1,11 +1,80 @@
 package sopt.org.millie.core.designsystem.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//Brand Colors
+val MillieYellow = Color(0xFFFFEB60)
+val SubYellow = Color(0xFFFFB904)
+val MilliePurple = Color(0xFFA451F7)
+val SubPurple = Color(0xFF534165)
+val AudioColor = Color(0xFFD16686)
+val AudioColor2 = Color(0xFF2C9285)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+//Greyscale Colors
+val White = Color(0xFFFFFFFF)
+val LightGrey1 = Color(0xFFF5F4F3)
+val LightGrey2 = Color(0xFFDFDFDF)
+val LightGrey3 = Color(0xFFC1C1C1)
+val Grey1 = Color(0xFFA5A5A5)
+val Grey2 = Color(0xFF8B8B8B)
+val Grey3 = Color(0xFF6F6F6F)
+val Grey4 = Color(0xFF555555)
+val DarkGrey1 = Color(0xFF3D3D3D)
+val DarkGrey2 = Color(0xFF242424)
+val Black = Color(0xFF000000)
+
+//Background Colors
+val Background = Color(0xFFFEFEFE)
+
+@Immutable
+data class DiveColors(
+    val millieYellow: Color = MillieYellow,
+    val subYellow: Color = SubYellow,
+    val milliePurple: Color = MilliePurple,
+    val subPurple: Color = SubPurple,
+    val audioColor: Color = AudioColor,
+    val audioColor2: Color = AudioColor2,
+
+    val white: Color = White,
+    val lightGrey1: Color = LightGrey1,
+    val lightGrey2: Color = LightGrey2,
+    val lightGrey3: Color = LightGrey3,
+    val grey1: Color = Grey1,
+    val grey2: Color = Grey2,
+    val grey3: Color = Grey3,
+    val grey4: Color = Grey4,
+    val darkGrey1: Color = DarkGrey1,
+    val darkGrey2: Color = DarkGrey2,
+    val black: Color = Black,
+
+    val background: Color = Background
+
+)
+
+val defaultDiveColors = DiveColors(
+    millieYellow = MillieYellow,
+    subYellow = SubYellow,
+    milliePurple = MilliePurple,
+    subPurple = SubPurple,
+    audioColor = AudioColor,
+    audioColor2 = AudioColor2,
+
+    white = White,
+    lightGrey1 = LightGrey1,
+    lightGrey2 = LightGrey2,
+    lightGrey3 = LightGrey3,
+    grey1 = Grey1,
+    grey2 = Grey2,
+    grey3 = Grey3,
+    grey4 = Grey4,
+    darkGrey1 = DarkGrey1,
+    darkGrey2 = DarkGrey2,
+    black = Black,
+
+    background = Background
+)
+
+val localDiveColorsProvider = staticCompositionLocalOf { defaultDiveColors }
--- app/src/main/java/sopt/org/millie/core/designsystem/theme/Type.kt
+++ app/src/main/java/sopt/org/millie/core/designsystem/theme/Type.kt
@@ -1,36 +1,146 @@
 package sopt.org.millie.core.designsystem.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import sopt.org.millie.R
 
-// Set of Material typography styles to start with
-val Typography =
-    Typography(
-        bodyLarge =
-            TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp,
-            ),
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+
+object PretendardFont {
+    val bold = FontFamily(Font(R.font.noto_sans_kr_bold))
+    val regular = FontFamily(Font(R.font.noto_sans_kr_regular))
+}
+
+sealed interface TypographyTokens{
+    @Immutable
+    data class Title(
+        val headLine: TextStyle,
+        val subHead5: TextStyle,
+        val subHead4: TextStyle,
+        val subHead3: TextStyle,
+        val subHead2: TextStyle,
+        val subHead1: TextStyle
+    )
+
+    @Immutable
+    data class Body(
+        val body3: TextStyle,
+        val body2: TextStyle,
+        val body1: TextStyle,
+        val subBody2: TextStyle,
+        val subBody1: TextStyle,
+        val caption4: TextStyle,
+        val caption3: TextStyle,
+        val caption2: TextStyle,
+        val caption1: TextStyle
+    )
+}
+
+@Immutable
+data class DiveTypography(
+    val title: TypographyTokens.Title,
+    val body: TypographyTokens.Body
+)
+
+
+val defaultDiveTypography = DiveTypography(
+    title = TypographyTokens.Title(
+        headLine = TextStyle(
+            fontFamily = PretendardFont.bold,
+            fontSize = 20.sp,
+            lineHeight = 28.sp, // 120%
+            letterSpacing = (-1).sp
+        ),
+        subHead5 = TextStyle(
+            fontFamily = PretendardFont.bold,
+            fontSize = 18.sp,
+            lineHeight = 28.sp, // 120%
+            letterSpacing = (-1).sp
+        ),
+        subHead4 = TextStyle(
+            fontFamily = PretendardFont.bold,
+            fontSize = 16.sp,
+            lineHeight = 22.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        subHead3 = TextStyle(
+            fontFamily = PretendardFont.bold,
+            fontSize = 15.sp,
+            lineHeight = 20.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        subHead2 = TextStyle(
+            fontFamily = PretendardFont.bold,
+            fontSize = 14.sp,
+            lineHeight = 20.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        subHead1 = TextStyle(
+            fontFamily = PretendardFont.bold,
+            fontSize = 12.sp,
+            lineHeight = 16.sp, // 120%
+            letterSpacing = (-0.6).sp
+        )
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    body = TypographyTokens.Body(
+        body3 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 16.sp,
+            lineHeight = 24.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        body2 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 15.sp,
+            lineHeight = 20.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        body1 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 14.sp,
+            lineHeight = 22.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        subBody2 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 13.sp,
+            lineHeight = 18.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        subBody1 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 11.sp,
+            lineHeight = 18.sp, // 120%
+            letterSpacing = 0.sp
+        ),
+        caption4 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 8.sp,
+            lineHeight = 10.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        caption3 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 12.sp,
+            lineHeight = 18.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        caption2 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 10.sp,
+            lineHeight = 18.sp, // 120%
+            letterSpacing = (-0.6).sp
+        ),
+        caption1 = TextStyle(
+            fontFamily = PretendardFont.regular,
+            fontSize = 9.sp,
+            lineHeight = 14.sp, // 120%
+            letterSpacing = (-0.6).sp
+        )
     )
-     */
-    )
+)
+
+val localDiveTypographyProvider = staticCompositionLocalOf { defaultDiveTypography }
