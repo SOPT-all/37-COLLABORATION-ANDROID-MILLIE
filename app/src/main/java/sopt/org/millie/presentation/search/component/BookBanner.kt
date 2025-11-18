f636b96809805792e618f4ddb3bf5b7a6721c146
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-18 16:16:08 +0900

    [feat/#8] 배너 구현

---
+++ app/src/main/java/sopt/org/millie/presentation/search/component/BookBanner.kt
@@ -0,0 +1,40 @@
+package sopt.org.millie.presentation.search.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import sopt.org.millie.R
+
+@Composable
+fun BookBanner(
+    bookBannerUri: String,
+    modifier: Modifier = Modifier
+) {
+    Box (
+        modifier = modifier
+            .width(311.dp)
+            .height(84.dp)
+            .clip(RoundedCornerShape(11.dp))
+    ){
+        AsyncImage(
+            model = bookBannerUri,
+            contentDescription = stringResource(R.string.book_banner),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BookBannerPreview() {
+    BookBanner("")
+}
