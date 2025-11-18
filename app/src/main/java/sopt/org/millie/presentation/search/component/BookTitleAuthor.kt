a6c571eec2a88a61d0fdd29d882437b16be6ce08
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-18 12:48:15 +0900

    [feat/#8] 책 제목, 저자 텍스트 구현

---
+++ app/src/main/java/sopt/org/millie/presentation/search/component/BookTitleAuthor.kt
@@ -0,0 +1,36 @@
+package sopt.org.millie.presentation.search.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import sopt.org.millie.core.designsystem.theme.MillieTheme
+
+@Composable
+fun BookTitleAuthor(
+    title: String,
+    author: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Text(
+            text = title,
+            color = MillieTheme.colors.black,
+            style = MillieTheme.typography.title.subHead3,
+        )
+        Text(
+            text = author,
+            color = MillieTheme.colors.gray3,
+            style = MillieTheme.typography.body.subBody2,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BookTitleAuthorPreview() {
+    BookTitleAuthor("홍학의 자리", "정해연")
+}
