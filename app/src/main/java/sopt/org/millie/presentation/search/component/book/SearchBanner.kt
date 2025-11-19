47402dbfb39a9a5840628a12aa733c60cf71547e
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-19 22:55:23 +0900

    [feat/#8] SearchBanner 수정

--- app/src/main/java/sopt/org/millie/presentation/search/component/book/SearchBanner.kt
+++ app/src/main/java/sopt/org/millie/presentation/search/component/book/SearchBanner.kt
@@ -1,29 +1,63 @@
 package sopt.org.millie.presentation.search.component.book
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import sopt.org.millie.core.designsystem.theme.MillieTheme
 
 @Composable
 fun SearchBanner(
+    bannerTitle: String,
+    bannerContent: String,
     bannerUrl: String,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier.fillMaxWidth()
-            .clip(RoundedCornerShape(11.dp)),
+    Row (
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(11.dp))
+            .background(color = MillieTheme.colors.bannerColor)
+            .padding(start = 7.dp, end = 18.dp),
     ) {
+        Column (
+            modifier = Modifier
+                .weight(1f)
+                .padding(bottom = 23.dp, end = 16.dp)
+        ){
+            Text(
+                text = bannerTitle,
+                modifier = Modifier.padding(top = 21.dp),
+                color = MillieTheme.colors.darkGray1,
+                style = MillieTheme.typography.title.subHead4
+            )
+
+            Text(
+                text = bannerContent,
+                color = MillieTheme.colors.gray3,
+                style = MillieTheme.typography.body.body1
+            )
+        }
+
         AsyncImage(
             model = bannerUrl,
-            contentDescription = "배너",
-            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = Modifier
+                .size(width = 64.dp, height = 70.dp)
+                .align(Alignment.Bottom),
+            contentScale = ContentScale.Crop
         )
     }
 }
@@ -32,6 +66,8 @@ fun SearchBanner(
 @Composable
 private fun BookBannerPreview() {
     SearchBanner(
+        bannerTitle = "《홍학의 자리》읽을 준비!",
+        bannerContent = "  ‘이 책'부터 읽어야 재미가 2배",
         bannerUrl = "",
     )
 }
