3a58768ead791ad339e333daa72667935c56fe51
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-21 18:22:39 +0900

    [mod/#21] libraryitem 간격 수정

--- app/src/main/java/sopt/org/millie/presentation/search/library/SearchLibraryScreen.kt
+++ app/src/main/java/sopt/org/millie/presentation/search/library/SearchLibraryScreen.kt
@@ -39,7 +39,7 @@ fun SearchLibraryScreen(
 
         Row(
             modifier = modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.spacedBy(19.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
             libraryList.forEach {
