89246bebed81e9ec96688e714a3202ad143c8a84
Kim Nahyeon <nhyeonii@naver.com>
2025-11-21 18:42:02 +0900

    [feat/#20] 바로가기 섹션 구현

---
+++ app/src/main/java/sopt/org/millie/presentation/home/component/item/QuickLink.kt
@@ -0,0 +1,57 @@
+package sopt.org.millie.presentation.home.component.item
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import sopt.org.millie.core.designsystem.theme.MillieTheme
+import sopt.org.millie.presentation.home.component.tag.CategoryTag
+import sopt.org.millie.presentation.home.constants.HomeConstants
+
+@Composable
+fun QuickLinks(
+    modifier: Modifier = Modifier,
+) {
+    val tags = remember { HomeConstants.QUICK_LINK_TAGS }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+    ) {
+        HomeTitleItem(
+            title = "바로가기",
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .padding(bottom = 12.dp),
+        )
+
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(15.dp),
+            contentPadding = PaddingValues(horizontal = 24.dp),
+        ) {
+            items(tags) { tag ->
+                CategoryTag(
+                    iconRes = tag.iconRes,
+                    title = tag.title,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun QuickLinksPreview() {
+    MillieTheme {
+        QuickLinks()
+    }
+}
