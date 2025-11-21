8ad4deb6ddb3c38b66299d1f49f4032eb6fdb16f
너의이름 <qkrcksal1052@gmail.com>
2025-11-21 22:19:29 +0900

    [feat/#23] Search Post 뷰 구현

---
+++ app/src/main/java/sopt/org/millie/presentation/search/post/SearchPostButton.kt
@@ -0,0 +1,34 @@
+package sopt.org.millie.presentation.search.post
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import sopt.org.millie.core.designsystem.component.button.MillieButton
+import sopt.org.millie.core.designsystem.theme.MillieTheme
+
+@Composable
+fun SearchPostButton(
+    modifier: Modifier = Modifier,
+){
+    MillieButton (
+        onClick = {} ,
+        buttonBackgroundColor = MillieTheme.colors.lightGray1,
+        buttonText = "전체보기",
+        buttonTextStyle = MillieTheme.typography.body.subBody2,
+        buttonTextColor = MillieTheme.colors.black,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 2.dp),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchPostScreenPreview(){
+    MillieTheme {
+        SearchPostButton()
+    }
+}
---
+++ app/src/main/java/sopt/org/millie/presentation/search/post/SearchPostTitle.kt
@@ -0,0 +1,30 @@
+package sopt.org.millie.presentation.search.post
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import sopt.org.millie.core.designsystem.theme.MillieTheme
+import sopt.org.millie.presentation.search.component.SearchTitle
+import sopt.org.millie.presentation.search.post.SearchPostScreen
+
+@Composable
+fun SearchPostTitle(
+    modifier: Modifier = Modifier,
+){
+    SearchTitle(
+        title = "포스트",
+        count = 736,
+        modifier = modifier
+            .padding(vertical = 10.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchPostScreenPreview(){
+    MillieTheme {
+        SearchPostTitle()
+    }
+}
