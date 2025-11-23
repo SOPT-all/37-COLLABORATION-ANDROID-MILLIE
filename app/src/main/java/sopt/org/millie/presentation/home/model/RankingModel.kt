c986e14e20a7133f377cb7fad50c252accd31680
Kim Nahyeon <nhyeonii@naver.com>
2025-11-23 14:03:36 +0900

    [chore/#19] 패키지 변경 및 이동

--- app/src/main/java/sopt/org/millie/presentation/home/model/RankingModel.kt
+++
@@ -1,9 +0,0 @@
-package sopt.org.millie.presentation.home.model
-
-import androidx.annotation.DrawableRes
-
-data class RankingModel(
-    val rank: Int,
-    val bookTitle: String,
-    @DrawableRes val rankingIcon: Int,
-)
