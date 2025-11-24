38921e3e026367f9b2608dad2f27ccb34b296c66
Kim Nahyeon <nhyeonii@naver.com>
2025-11-25 02:10:46 +0900

    [feat/#30] SearchDataSource 및 SearchDataSourceImpl 구현

--- app/src/main/java/sopt/org/millie/data/datasource/SearchDataSource.kt
+++ app/src/main/java/sopt/org/millie/data/datasource/SearchDataSource.kt
@@ -1,4 +1,8 @@
 package sopt.org.millie.data.datasource
 
-interface DummyDataSource {
+import sopt.org.millie.core.network.BaseResponse
+import sopt.org.millie.data.dto.response.CategoryResponseDto
+
+interface SearchDataSource {
+    suspend fun getCategories(): BaseResponse<List<CategoryResponseDto>>
 }
--- app/src/main/java/sopt/org/millie/data/datasourceimpl/SearchDataSourceImpl.kt
+++ app/src/main/java/sopt/org/millie/data/datasourceimpl/SearchDataSourceImpl.kt
@@ -1,4 +1,16 @@
 package sopt.org.millie.data.datasourceimpl
 
-class DummyDataSourceImpl {
+import sopt.org.millie.core.network.BaseResponse
+import sopt.org.millie.data.datasource.SearchDataSource
+import sopt.org.millie.data.dto.response.CategoryResponseDto
+import sopt.org.millie.data.service.SearchService
+import javax.inject.Inject
+
+class SearchDataSourceImpl
+    @Inject
+    constructor(
+    private val searchService: SearchService,
+) : SearchDataSource {
+    override suspend fun getCategories(): BaseResponse<List<CategoryResponseDto>> =
+        searchService.getCategories()
 }
