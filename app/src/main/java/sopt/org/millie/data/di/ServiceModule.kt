2148ed55c69f6d09587990bccea8e94d8de2cbca
Kim Nahyeon <nhyeonii@naver.com>
2025-11-25 02:10:12 +0900

    [feat/#30] SearchService 및 ServiceModule 구현

--- app/src/main/java/sopt/org/millie/data/di/ServiceModule.kt
+++ app/src/main/java/sopt/org/millie/data/di/ServiceModule.kt
@@ -1,3 +1,17 @@
 package sopt.org.millie.data.di
 
-object ServiceModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import sopt.org.millie.data.service.SearchService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun provideSearchService(retrofit: Retrofit): SearchService = retrofit.create(SearchService::class.java)
+}
--- app/src/main/java/sopt/org/millie/data/service/SearchService.kt
+++ app/src/main/java/sopt/org/millie/data/service/SearchService.kt
@@ -1,1 +1,10 @@
 package sopt.org.millie.data.service
+
+import retrofit2.http.GET
+import sopt.org.millie.core.network.BaseResponse
+import sopt.org.millie.data.dto.response.CategoryResponseDto
+
+interface SearchService {
+    @GET("categories")
+    suspend fun getCategories(): BaseResponse<List<CategoryResponseDto>>
+}
