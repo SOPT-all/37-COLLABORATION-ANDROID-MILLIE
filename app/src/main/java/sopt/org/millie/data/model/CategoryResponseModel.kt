f0eccc905a91fa6dc5d52addbd7598020ba2d3b2
Kim Nahyeon <nhyeonii@naver.com>
2025-11-25 02:09:38 +0900

    [feat/#30] CategoryResponseDto, Model 생성

--- app/src/main/java/sopt/org/millie/data/model/CategoryResponseModel.kt
+++ app/src/main/java/sopt/org/millie/data/model/CategoryResponseModel.kt
@@ -1,4 +1,18 @@
 package sopt.org.millie.data.model
 
-class CategoryResponseModel {
-}
+import sopt.org.millie.data.dto.response.CategoryResponseDto
+
+data class CategoryResponseModel(
+    val categoryId: Long,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+)
+
+fun CategoryResponseDto.toModel() =
+    CategoryResponseModel(
+        categoryId = categoryId,
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+    )
