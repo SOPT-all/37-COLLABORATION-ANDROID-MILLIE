d7878fab3a66b21aa8d44200975f3051a00ea994
Kim Nahyeon <nhyeonii@naver.com>
2025-11-24 16:29:38 +0900

    [chore/#20] lint format

--- app/src/main/java/sopt/org/millie/presentation/home/component/item/QuickLink.kt
+++ app/src/main/java/sopt/org/millie/presentation/home/component/item/QuickLink.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +35,7 @@ fun QuickLinks(
             horizontalArrangement = Arrangement.spacedBy(15.dp),
             contentPadding = PaddingValues(horizontal = 24.dp),
         ) {
-            items(HomeConstants.QUICK_LINK_TAGS ) { tag ->
+            items(HomeConstants.QUICK_LINK_TAGS) { tag ->
                 CategoryTag(
                     iconRes = tag.iconRes,
                     title = tag.title,
