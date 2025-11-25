e79f46b927bd9ae82e1bd9e8e7e4baeefc7dfe4f
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-25 21:55:53 +0900

    [feat/#33] service 추가

--- app/src/main/java/sopt/org/millie/data/service/SearchService.kt
+++ app/src/main/java/sopt/org/millie/data/service/SearchService.kt
@@ -1,10 +1,17 @@
 package sopt.org.millie.data.service
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 import sopt.org.millie.core.network.BaseResponse
+import sopt.org.millie.data.dto.response.BookSearchResponseDto
 import sopt.org.millie.data.dto.response.CategoryResponseDto
 
 interface SearchService {
     @GET("categories")
     suspend fun getCategories(): BaseResponse<List<CategoryResponseDto>>
+
+    @GET("books")
+    suspend fun getBooks(
+        @Query("keyword") keyword: String,
+    ): BaseResponse<List<BookSearchResponseDto>>
 }
