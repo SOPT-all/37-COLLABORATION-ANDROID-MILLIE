ab0895a3c55a2ce26b9b313dac6cbc9dcf36c242
Kim Nahyeon <nhyeonii@naver.com>
2025-11-25 21:57:03 +0900

    [chore/#39] lint format

--- app/src/main/java/sopt/org/millie/data/repositoryimpl/SearchRepositoryImpl.kt
+++ app/src/main/java/sopt/org/millie/data/repositoryimpl/SearchRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package sopt.org.millie.data.repositoryimpl
 
-import kotlinx.collections.immutable.persistentListOf
 import sopt.org.millie.core.util.suspendRunCatching
 import sopt.org.millie.data.datasource.SearchDataSource
 import sopt.org.millie.data.model.CategoryResponseModel
