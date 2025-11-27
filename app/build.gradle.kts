2c5820188d60deac80f776b19ac138266fa3990f
Kim Nahyeon <nhyeonii@naver.com>
2025-11-28 01:03:34 +0900

    [feat/#50] animation 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
 
     // Hilt
     implementation(libs.hilt.android)
+    implementation(libs.androidx.compose.animation)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
