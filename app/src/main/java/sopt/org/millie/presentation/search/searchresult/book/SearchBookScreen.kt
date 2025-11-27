e6faaa8fa18ec26fc37672532e121f8192df2c28
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-28 00:00:20 +0900

    [mod/#46] LazyVerticalGrid로 수정

--- app/src/main/java/sopt/org/millie/presentation/search/searchresult/book/SearchBookScreen.kt
+++ app/src/main/java/sopt/org/millie/presentation/search/searchresult/book/SearchBookScreen.kt
@@ -4,14 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,59 +28,55 @@ fun SearchBookScreen(
     onBookItemClick: (Long) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val listState = rememberLazyListState()
-
-    LaunchedEffect(bookList) {
-        listState.scrollToItem(0)
-    }
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(top = 24.dp, bottom = 37.dp),
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        contentPadding = PaddingValues(horizontal = 17.dp, vertical = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
-        SearchTitle(
-            title = "도서",
-            count = bookList.size,
-            modifier = Modifier.padding(start = 17.dp),
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        LazyRow(
-            modifier = Modifier,
-            state = listState,
-            contentPadding = PaddingValues(horizontal = 17.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        item(
+            span = { GridItemSpan(maxLineSpan) },
         ) {
-            items(
-                items = bookList,
-                key = { it.bookId },
-            ) { book ->
-                SearchBookItem(
-                    bookId = book.bookId,
-                    bookCoverImageUrl = book.bookCoverImageUrl,
-                    bookTitle = book.bookTitle,
-                    bookAuthor = book.bookAuthor,
-                    completionRate = book.completionRate,
-                    completionTime = book.completionTime,
-                    onBookItemClick = { onBookItemClick(book.bookId) },
-                    isAudiobook = book.isAudiobook,
-                    voiceActor = book.voiceActor,
-                )
-            }
+            SearchTitle(
+                title = "도서",
+                count = bookList.size,
+            )
         }
 
-        Spacer(modifier = Modifier.weight(1f))
+        items(
+            items = bookList,
+            key = { it.bookId },
+        ) { book ->
+            SearchBookItem(
+                bookId = book.bookId,
+                bookCoverImageUrl = book.bookCoverImageUrl,
+                bookTitle = book.bookTitle,
+                bookAuthor = book.bookAuthor,
+                completionRate = book.completionRate,
+                completionTime = book.completionTime,
+                onBookItemClick = { onBookItemClick(book.bookId) },
+                isAudiobook = book.isAudiobook,
+                voiceActor = book.voiceActor,
+            )
+        }
 
-        SearchBanner(
-            bannerTitle = searchBanner.bannerTitle,
-            bannerContent = searchBanner.bannerContent,
-            bannerImageUrl = searchBanner.bannerImageUrl,
-            modifier = Modifier.padding(horizontal = 25.dp),
-        )
+        item(
+            span = { GridItemSpan(maxLineSpan) },
+        ) {
+            Column {
+                Spacer(modifier = Modifier.height(50.dp))
 
-        Spacer(modifier = Modifier.height(14.dp))
+                SearchBanner(
+                    bannerTitle = searchBanner.bannerTitle,
+                    bannerContent = searchBanner.bannerContent,
+                    bannerImageUrl = searchBanner.bannerImageUrl,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp),
+                )
+
+                Spacer(modifier = Modifier.height(17.dp))
+            }
+        }
     }
 }
 
